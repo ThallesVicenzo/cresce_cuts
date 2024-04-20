5894241f4fe7da13fb0b457f83af5d078fcb48bd
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-19 21:58:05 -0300

    feat: adds SplashPage

---
+++ lib/features/splash/presenter/pages/splash_page.dart
@@ -0,0 +1,68 @@
+import 'package:cresce_cuts/core/page_state.dart';
+import 'package:design_system/enums/app_images.dart';
+import 'package:design_system/pages/default_erro_page.dart';
+import 'package:design_system/widgets/asset_image_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../../../core/main_routes.dart';
+import '../controllers/splash_controller.dart';
+
+class SplashPage extends StatefulWidget {
+  final SplashController controller;
+  const SplashPage({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  State<SplashPage> createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashPage> {
+  @override
+  void initState() {
+    widget.controller.getProducts();
+    widget.controller.state.addListener(listenableErrorState);
+    super.initState();
+  }
+
+  void listenableErrorState() {
+    final state = widget.controller.state.value;
+
+    if (state is ErrorState) {
+      Modular.to.pushNamed(
+        MainRoutes.defaultError.route,
+        arguments: ErrorPageParams(
+          errorlog: state.asError.message,
+          code: state.asError.code.toString(),
+          onButtonPressed: (_) {
+            widget.controller.getProducts();
+          },
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ValueListenableBuilder<double>(
+        valueListenable: widget.controller.opacity,
+        builder: (context, value, child) => AnimatedOpacity(
+          opacity: value,
+          duration: kThemeAnimationDuration,
+          onEnd: () {
+            widget.controller.onAnimationEd(value);
+          },
+          child: const Center(
+            child: AssetImageWidget(
+              image: AppImage.logo,
+              size: Size(300, 300),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
