6c02c1c47b8fdc45347d23bd967d006e80792507
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 15:32:22 -0300

    feat: adds DefaultAppBar

---
+++ design_system/lib/widgets/app_bar/default_app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../colors/colors_palette.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultAppBar({
+    super.key,
+    this.height = kToolbarHeight,
+    required this.title,
+    required this.hasLeading,
+    this.leading,
+  });
+
+  final double height;
+  final String title;
+  final bool hasLeading;
+  final void Function()? leading;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: hasLeading,
+      leading: Visibility(
+        visible: hasLeading,
+        child: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: ColorsPalette.skyBlue,
+          ),
+          onPressed: leading ??
+              () {
+                Navigator.pop(context);
+              },
+        ),
+      ),
+      centerTitle: true,
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
