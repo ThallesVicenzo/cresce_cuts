f73b6fafc26f68efeb20072ca7fc2ad5a03ba0cb
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 13:20:42 -0300

    feat: adds localization delegates

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:design_system/widgets/colors/ui_overlay_color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'core/main_routes.dart';
@@ -13,6 +15,13 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       title: 'Cresce Cuts',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+      ],
       routerConfig: Modular.routerConfig,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
