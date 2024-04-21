8a46c978e44a7b3365a0c3bac6996dc82d9e9606
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 13:20:21 -0300

    feat: adds more values in product entity

--- lib/features/splash/domain/entities/product_entity.dart
+++ lib/features/splash/domain/entities/product_entity.dart
@@ -1,17 +1,27 @@
 class ProductEntity {
   final int id;
   final String title;
-  final dynamic price;
+  final double initialPrice;
+  final double finalPrice;
+  final bool isActive;
   final String category;
   final String description;
   final String image;
+  final String discount;
+  final DateTime activationDate;
+  final DateTime inactivationDate;
 
   ProductEntity({
     required this.id,
     required this.title,
-    required this.price,
+    required this.initialPrice,
+    required this.finalPrice,
+    required this.isActive,
     required this.category,
     required this.description,
     required this.image,
+    required this.discount,
+    required this.activationDate,
+    required this.inactivationDate,
   });
 }
--- lib/features/splash/infra/models/product_model.dart
+++ lib/features/splash/infra/models/product_model.dart
@@ -5,18 +5,28 @@ class ProductModel extends ProductEntity {
   ProductModel({
     required super.id,
     required super.title,
-    required super.price,
+    required super.initialPrice,
+    required super.finalPrice,
+    required super.isActive,
     required super.category,
     required super.description,
     required super.image,
+    required super.discount,
+    required super.activationDate,
+    required super.inactivationDate,
   });
 
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
         id: json.getValue('id'),
         title: json.getValue('title'),
-        price: json.getValue('price'),
+        initialPrice: json.getValue('initialPrice'),
+        finalPrice: json.getValue('finalPrice'),
+        isActive: json.getValue('isActive'),
         category: json.getValue('category'),
         description: json.getValue('description'),
         image: json.getValue('image'),
+        discount: json.getValue('discount'),
+        activationDate: DateTime.parse(json.getValue('activationDate')),
+        inactivationDate: DateTime.parse(json.getValue('inactivationDate')),
       );
 }
