ac393c95b51be78b23ca56ad6bcaf89243c0e6c3
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 15:46:15 -0300

    feat: adds SelectDiscountDialog

---
+++ lib/features/home/presenter/widgets/select_discount_dialog.dart
@@ -0,0 +1,64 @@
+import 'package:cresce_cuts/core/enums/discount_types.dart';
+import 'package:cresce_cuts/features/home/home_routes.dart';
+import 'package:design_system/widgets/buttons/default_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'select_discount_type_widget.dart';
+
+class SelectDiscountDialog extends StatelessWidget {
+  const SelectDiscountDialog({
+    super.key,
+    required this.discountType,
+  });
+
+  final ValueNotifier<DiscountType> discountType;
+
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(
+      builder: (context, setState) => AlertDialog(
+        surfaceTintColor: Colors.white,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Por favor, escolha o tipo de desconto',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        content: SelectedDiscountTypeWidget(
+          discountType: discountType.value,
+          onTap: (value) {
+            setState(() {
+              discountType.value = value;
+            });
+          },
+        ),
+        actions: [
+          DefaultButton(
+            size: const Size(60, 40),
+            title: 'Aceitar',
+            onPressed: () {
+              Modular.to.pushNamed(
+                HomeRoutes.inputDiscountPage.path,
+                arguments: {
+                  'discount': discountType.value,
+                },
+              );
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              Modular.to.pop();
+            },
+            child: const Text(
+              'Voltar',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
