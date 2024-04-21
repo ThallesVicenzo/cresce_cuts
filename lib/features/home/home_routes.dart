1a7f36217ced20b366efcae547fbb42c42ca3403
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 15:46:57 -0300

    feat: adds inputDiscountPage route

--- lib/features/home/home_routes.dart
+++ lib/features/home/home_routes.dart
@@ -1,7 +1,8 @@
 import 'package:cresce_cuts/core/main_routes.dart';
 
 enum HomeRoutes {
-  homePage('/');
+  homePage('/'),
+  inputDiscountPage('/input-discount');
 
   const HomeRoutes(this.route);
 
