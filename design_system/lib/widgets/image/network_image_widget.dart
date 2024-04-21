6f5473e7d391d78a6093215a0fd6ea60d8001350
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 13:17:54 -0300

    feat: adds NetworkImageWidget

---
+++ design_system/lib/widgets/image/network_image_widget.dart
@@ -0,0 +1,38 @@
+import 'package:design_system/widgets/colors/colors_palette.dart';
+import 'package:flutter/cupertino.dart';
+
+class NetworkImageWidget extends StatelessWidget {
+  final String url;
+  final Size size;
+  final BoxFit fit;
+  final Alignment alignment;
+  final String? package;
+
+  const NetworkImageWidget({
+    super.key,
+    required this.url,
+    this.size = const Size(24, 24),
+    this.fit = BoxFit.contain,
+    this.alignment = Alignment.center,
+    this.package = 'design_system',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        border: Border.all(
+          color: ColorsPalette.defaultBorder,
+        ),
+      ),
+      child: Image.network(
+        url,
+        width: size.width,
+        height: size.height,
+        fit: fit,
+        alignment: alignment,
+      ),
+    );
+  }
+}
