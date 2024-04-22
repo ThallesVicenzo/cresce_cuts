129c7652cb354b6099e9302f55e6328940adeb2e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 22:42:18 -0300

    feat: adds finals file

---
+++ design_system/lib/widgets/utils/values/finals.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../../colors/colors_palette.dart';
+
+final outlineBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(10),
+  borderSide: const BorderSide(color: ColorsPalette.defaultBorder),
+);
