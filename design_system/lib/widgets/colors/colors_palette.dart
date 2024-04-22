c63f79fc9c33ba06d2da2cd4dd0b350f96fd4800
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-21 23:09:23 -0300

    feat: adds a new color

--- design_system/lib/widgets/colors/colors_palette.dart
+++ design_system/lib/widgets/colors/colors_palette.dart
@@ -4,6 +4,7 @@ class ColorsPalette extends Color {
   static const Color backgroundColor = Colors.white;
   static const Color defaultBorder = Color(0xffCFD8DC);
   static const Color skyBlue = Color(0xff007FBA);
+  static const Color lightDark = Color(0xff455A64);
 
   ColorsPalette(super.value);
 }
