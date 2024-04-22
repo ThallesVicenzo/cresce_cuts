79c8626068bc88527113c9b4e8809fc0e23682d6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-22 14:34:17 -0300

    feat: adds FloatingButton

---
+++ design_system/lib/widgets/buttons/floating_button.dart
@@ -0,0 +1,52 @@
+import 'package:design_system/widgets/colors/colors_palette.dart';
+import 'package:flutter/material.dart';
+
+class FloatingButton extends StatelessWidget {
+  const FloatingButton(
+      {super.key, this.size, required this.title, required this.onPressed});
+
+  final Size? size;
+  final String title;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      height: 80,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 2,
+            color: ColorsPalette.defaultBorder,
+          ),
+        ],
+      ),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: TextButton(
+          onPressed: onPressed,
+          child: Container(
+            height: size?.height,
+            width: size?.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: ColorsPalette.skyBlue,
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
