c3822fc71b72a95d495a8144ec5211a5b4167a18
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-23 17:49:53 -0300

    feat: adds edit product domain

--- lib/features/input_discount/domain/use_case/edit_product_use_case.dart
+++ lib/features/input_discount/domain/use_case/edit_product_use_case.dart
@@ -1,22 +1,9 @@
 import 'package:cresce_cuts/core/domain/errors/response_error.dart';
 import 'package:cresce_cuts/core/enums/discount_types.dart';
+import 'package:cresce_cuts/features/input_discount/domain/repository/send_product_repository.dart';
 import 'package:either_dart/either.dart';
 
-abstract class SendProductRepository {
-  Future<Either<ResponseError, String?>> call({
-    required String discountTitle,
-    required String description,
-    required DiscountType discountType,
-    required double initialPrice,
-    required double finalPrice,
-    required double? price,
-    required DateTime activationDate,
-    required DateTime inactivationDate,
-    required String image,
-  });
-}
-
-abstract class EditProductRepository {
+abstract class EditProductUseCase {
   Future<Either<ResponseError, String?>> call({
     required String discountTitle,
     required String description,
@@ -29,4 +16,37 @@ abstract class EditProductRepository {
     required String image,
     required int index,
   });
+}
+
+class EditProductUsecaseImpl implements EditProductUseCase {
+  final EditProductRepository repository;
+
+  EditProductUsecaseImpl({required this.repository});
+
+  @override
+  Future<Either<ResponseError, String?>> call({
+    required String discountTitle,
+    required String description,
+    required DiscountType discountType,
+    required double initialPrice,
+    required double finalPrice,
+    required double? price,
+    required DateTime activationDate,
+    required DateTime inactivationDate,
+    required String image,
+    required int index,
+  }) async {
+    return await repository.call(
+      discountTitle: discountTitle,
+      description: description,
+      discountType: discountType,
+      initialPrice: initialPrice,
+      finalPrice: finalPrice,
+      price: price,
+      activationDate: activationDate,
+      inactivationDate: inactivationDate,
+      image: image,
+      index: index,
+    );
+  }
 }
