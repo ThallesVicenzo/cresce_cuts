5a356a3436308bcfdbf8fb748dcf77ee4e14ad68
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-23 15:03:21 -0300

    feat: injects home module dependencies

--- lib/features/home/home_module.dart
+++ lib/features/home/home_module.dart
@@ -1,4 +1,9 @@
+import 'package:cresce_cuts/features/home/domain/repository/products_repository.dart';
+import 'package:cresce_cuts/features/home/domain/use_case/products_use_case.dart';
+import 'package:cresce_cuts/features/home/external/products_data_source_impl.dart';
 import 'package:cresce_cuts/features/home/home_routes.dart';
+import 'package:cresce_cuts/features/home/infra/data_source/products_data_source.dart';
+import 'package:cresce_cuts/features/home/infra/repository/products_repository_impl.dart';
 import 'package:cresce_cuts/features/home/presenter/controllers/home_controller.dart';
 import 'package:cresce_cuts/features/home/presenter/controllers/home_details_controller.dart';
 import 'package:cresce_cuts/features/home/presenter/pages/home_details_page.dart';
@@ -10,6 +15,13 @@ import '../../core/domain/entities/product_entity.dart';
 class HomeModule extends Module {
   @override
   void binds(Injector i) {
+    i.add<ProductsUseCase>(ProductsUsecaseImp.new);
+    i.add<ProductsRepository>(ProductsRepositoryImpl.new);
+    i.addInstance<ProductsDataSource>(ProductsDataSourceImpl(
+      storage: Modular.get(
+        key: 'storage',
+      ),
+    ));
     i.add<HomeController>(HomeController.new);
     i.add<HomeDetailsController>(HomeDetailsController.new);
   }
@@ -20,7 +32,6 @@ class HomeModule extends Module {
       HomeRoutes.homePage.route,
       child: (context) => HomePage(
         controller: context.read<HomeController>(),
-        products: r.args.data['products'] as List<ProductEntity>,
       ),
     );
     r.child(
