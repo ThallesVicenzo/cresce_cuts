ba01ff1bb32682b616128d3d73920387baf4d953
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-22 21:42:39 -0300

    docs: fixes wrong import path

--- lib/features/splash/external/products_data_source_impl.dart
+++ lib/features/splash/external/products_data_source_impl.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
+import 'package:cresce_cuts/core/domain/entities/product_entity.dart';
 import 'package:cresce_cuts/core/secure_storage/keys/secure_storage_keys.dart';
 import 'package:cresce_cuts/core/secure_storage/secure_storage.dart';
-import 'package:cresce_cuts/features/splash/domain/entities/product_entity.dart';
 import 'package:cresce_cuts/features/splash/infra/models/product_model.dart';
 
 import '../infra/data_source/products_data_source.dart';
