3597dd0a2f479a5ff6207e6d9706ae94d5d8b86e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-23 15:03:05 -0300

    feat: adds a Refresh to home list

--- lib/features/home/presenter/pages/home_page.dart
+++ lib/features/home/presenter/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:cresce_cuts/core/page_state.dart';
 import 'package:design_system/pages/default_erro_page.dart';
 import 'package:design_system/widgets/app_bar/default_app_bar.dart';
 import 'package:design_system/widgets/buttons/floating_button.dart';
+import 'package:design_system/widgets/colors/colors_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -14,11 +15,9 @@ import '../widgets/home_widgets/select_discount_dialog.dart';
 
 class HomePage extends StatefulWidget {
   final HomeController controller;
-  final List<ProductEntity> products;
   const HomePage({
     super.key,
     required this.controller,
-    required this.products,
   });
 
   @override
@@ -29,7 +28,7 @@ class _SplashScreen extends State<HomePage> {
   @override
   void initState() {
     widget.controller.state.addListener(listenableErrorState);
-    widget.controller.populateSwitchValue(widget.products);
+    widget.controller.callData();
     super.initState();
   }
 
@@ -57,53 +56,70 @@ class _SplashScreen extends State<HomePage> {
         title: 'Descontos',
         hasLeading: false,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            bottom: 40,
-            right: 20,
-            left: 20,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Visibility(
-                  visible: widget.products.isEmpty,
-                  child: const Center(
-                    heightFactor: 30,
-                    child: Text(
-                      'Você não possui produtos cadastrados para o desconto.',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
+      body: ValueListenableBuilder<PageState<List<ProductEntity>>>(
+          valueListenable: widget.controller.state,
+          builder: (context, state, child) {
+            if (state is LoadingState) {
+              return const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    color: ColorsPalette.skyBlue,
+                  ),
+                ],
+              );
+            }
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: 40,
+                right: 20,
+                left: 20,
+              ),
+              child: RefreshIndicator(
+                onRefresh: widget.controller.callData,
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Column(
+                    children: [
+                      Visibility(
+                        visible: state.asSuccess.isEmpty,
+                        child: const Center(
+                          heightFactor: 30,
+                          child: Text(
+                            'Você não possui produtos cadastrados para o desconto.',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        itemCount: state.asSuccess.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 16),
+                            child: DiscountInfo(
+                              controller: widget.controller,
+                              entity: state.asSuccess[index],
+                              index: index,
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      )
+                    ],
                   ),
                 ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  itemCount: widget.products.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: DiscountInfo(
-                        controller: widget.controller,
-                        entity: widget.products[index],
-                        index: index,
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 60,
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
+            );
+          }),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingButton(
         size: const Size(double.infinity, 56),
