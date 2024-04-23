d09f2621ea7c1e5d483d33c8c8fad854acd7b61a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-22 23:43:09 -0300

    feat: adds input discount domain

---
+++ lib/features/input_discount/domain/repository/send_product_repository.dart
@@ -0,0 +1,16 @@
+import 'package:cresce_cuts/core/domain/errors/response_error.dart';
+import 'package:cresce_cuts/core/enums/discount_types.dart';
+import 'package:either_dart/either.dart';
+
+abstract class SendProductRepository {
+  Future<Either<ResponseError, String?>> call({
+    required String discountTitle,
+    required String description,
+    required DiscountType discountType,
+    required double initialPrice,
+    required double finalPrice,
+    required DateTime activationDate,
+    required DateTime inactivationDate,
+    required String image,
+  });
+}
