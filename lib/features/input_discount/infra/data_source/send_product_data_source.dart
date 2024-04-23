95c51d1e2a3ce7bb3a579d83e8dbb1ab41bce12b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-23 17:50:17 -0300

    feat: adds edit product infra

--- lib/features/input_discount/infra/data_source/send_product_data_source.dart
+++ lib/features/input_discount/infra/data_source/send_product_data_source.dart
@@ -13,3 +13,18 @@ abstract class SendProductDataSource {
     required String image,
   });
 }
+
+abstract class EditProductDataSource {
+  Future<String?> call({
+    required String discountTitle,
+    required String description,
+    required DiscountType discountType,
+    required double initialPrice,
+    required double finalPrice,
+    required double? price,
+    required DateTime activationDate,
+    required DateTime inactivationDate,
+    required String image,
+    required int index,
+  });
+}
---
+++ lib/features/input_discount/infra/repository/edit_product_repository_impl.dart
@@ -0,0 +1,56 @@
+import 'package:cresce_cuts/core/client/client_http_exception.dart';
+import 'package:cresce_cuts/core/domain/errors/response_error.dart';
+import 'package:cresce_cuts/core/enums/discount_types.dart';
+import 'package:cresce_cuts/features/input_discount/domain/repository/send_product_repository.dart';
+import 'package:cresce_cuts/features/input_discount/infra/data_source/send_product_data_source.dart';
+import 'package:either_dart/either.dart';
+
+class EditProductRepositoryImpl implements EditProductRepository {
+  final EditProductDataSource dataSource;
+
+  EditProductRepositoryImpl({required this.dataSource});
+
+  @override
+  Future<Either<ResponseError, String?>> call({
+    required String discountTitle,
+    required String description,
+    required DiscountType discountType,
+    required double initialPrice,
+    required double finalPrice,
+    required double? price,
+    required DateTime activationDate,
+    required DateTime inactivationDate,
+    required String image,
+    required int index,
+  }) async {
+    try {
+      final data = await dataSource.call(
+        discountTitle: discountTitle,
+        description: description,
+        discountType: discountType,
+        price: price,
+        initialPrice: initialPrice,
+        finalPrice: finalPrice,
+        activationDate: activationDate,
+        inactivationDate: inactivationDate,
+        image: image,
+        index: index,
+      );
+      return Right(data);
+    } on ClientHttpException catch (e) {
+      return Left(
+        ResponseClientError(
+          exception: e,
+          errorCode: e.statusCode,
+          errorMessage: e.message,
+        ),
+      );
+    } catch (e) {
+      return Left(
+        ResponseGenericError(
+          error: e,
+        ),
+      );
+    }
+  }
+}
